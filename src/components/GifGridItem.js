5441652ecb50e19be5460732c274ae3d8034a49e
Ramiro Esaú Ramírez Alonzo <73205322+Eson97@users.noreply.github.com>
2021-05-02 00:28:01 -0500

    Share button added

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
+
+
 export const GifGridItem = ({title,url}) => {
+    
     return (
         <div className="card animate__animated animate__fadeInLeft">
             <img src={url} alt={title}/>
             <p>{title}</p>
+            <iframe title="share-button" src={`https://www.facebook.com/plugins/share_button.php?href=${url}&layout=button&size=large&width=103&height=28&appId`} width="103" height="28" style={{border:"none",overflow:"hidden",margin:"10px"}} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
         </div>
     )
 }
